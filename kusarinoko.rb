ef9564f370250546d878b88044ca7afce021c33e
tazro inutano orta <inut.mini@gmail.com>
2011-05-25 17:16:15 +0900

    there is not only one way in your life

--- kusarinoko.rb
+++ kusarinoko.rb
@@ -7,22 +7,60 @@ require "./nokosearch.rb"
 
 set :haml, :format => :html5
 
+before do
+	@searchdb = "./ksrnk_json/H_sapiens.json" # future: select with radio button or something
+end
+
+helpers do	
+	def querychecker(query)
+		watashiwoamariokorasenaihougaii = []
+		badletters = [";", "(", ")", "<", ">", "script", "alert", "xml"]
+		badletters.each do |bad|
+			if query.include?(bad)
+				watashiwoamariokorasenaihougaii.push(":(")
+			end
+		end
+		if query.length > 40 or query.length < 3
+			watashiwoamariokorasenaihougaii.push(":(")
+		end
+		if watashiwoamariokorasenaihougaii.length > 2
+			return "enemy"
+		elsif watashiwoamariokorasenaihougaii.length == 2
+			return "mistake"
+		else
+			return "ok"
+		end
+		open("query.log","a") { |f| f.puts(query) }	
+	end
+end
+
 get "/" do
 	haml :index
 end
 
 post "/result" do
 	
-	query = params[:query]
-	searchdb = "ksrnk_json/H_sapiens.json" # future: select with radio button or something
+	@query = params[:query]
+	pass unless querychecker(@query) == "enemy"
+	haml :bullshit
+end
+
+post "/result" do
 	
-	@query = query
-	@result = nokosearch(searchdb,query)
-	open("query.log","a") { |f| f.puts(query) }
+	@query = params[:query]
+	pass unless querychecker(@query) == "mistake"
+	haml :tryagain
+end
+
+post "/result" do
+	
+	@query = params[:query]	
+	@result = nokosearch(@searchdb, @query)
+	pass if @result == "no dataset found."
 	
 	haml :result
 end
 
 not_found do
-	redirect "/"
+	haml :not_found
 end
