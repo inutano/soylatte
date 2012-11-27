9a084344485d87acc83b32f9e47b8ef9a819ab4e
tazro inutano ohta <inutano@gmail.com>
2012-11-27 15:54:45 +0900

    written search section (fake code)

--- app.rb
+++ app.rb
@@ -4,8 +4,17 @@ require "sinatra"
 require "haml"
 require "sass"
 
-get %r{/$} do
+get "/" do
   haml :index
 end
 
-
+post "/search" do
+  query_raw = params[:query]
+  query = queryfilter(query_raw)
+  result = soy_search(query)
+  if result
+    haml :search
+  else
+    haml :search_failed
+  end
+end
