8cb3efcdd51795ea94be39f4d3a75616cbbbf0d0
tazro inutano ohta <inutano@gmail.com>
2012-11-27 16:15:52 +0900

    implemented query_filter and soy_search methods (fake)

--- app.rb
+++ app.rb
@@ -13,6 +13,14 @@ def logging(query)
   open(Logfile,"a"){|f| f.puts}
 end
 
+def query_filter(query_raw)
+  "cleaned query"
+end
+
+def soy_search(query)
+  [{:id => "SRP000001", :paper => "11111111"}, {:id => "ERP000001", :paper => "22222222"}] if query
+end
+
 set :haml, :format => :html5
 
 before do
@@ -25,7 +33,7 @@ end
 
 post "/search" do
   query_raw = params[:query]
-  query = queryfilter(query_raw)
+  query = query_filter(query_raw)
   result = soy_search(query)
   if result
     haml :search
