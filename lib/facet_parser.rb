3f8b87769406b81b8c4c9b5e6dbc1eede0e5837f
tazro inutano ohta <inutano@gmail.com>
2012-12-06 15:40:43 +0900

    implemented mock methods on FacetParser class

--- lib/facet_parser.rb
+++ lib/facet_parser.rb
@@ -2,8 +2,46 @@
 
 require "sra_metadata_parser"
 
-class FacetParser  
-  def initialize(runid)
+class FacetParser
+  def self.load_file
     
+  end
+  
+  def initialize(runid)
+    @runid = runid
+  end
+  
+  def studyid
+    "SRP000001"
+  end
+  
+  def taxonid
+    9606
+  end
+  
+  def study_type
+    1
+  end
+  
+  def instrument
+    "illumina HiSeq 2000"
+  end
+  
+  def full_text
+    "hogehoge"
+  end
+  
+  def paper
+    true
+  end
+  
+  def insert
+    { runid: @runid,
+      studyid: self.studyid,
+      taxonid: self.taxonid,
+      study_type: self.study_type,
+      instrument: self.instrument,
+      fulltext: self.fulltext,
+      paper: self.paper }
   end
 end
