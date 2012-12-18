96ae1c070e1a0efcffbcf952ca41fd2273886be4
tazro inutano ohta <inutano@gmail.com>
2012-12-18 12:07:47 +0900

    implse coding at mbsj2012

--- app.rb
+++ app.rb
@@ -6,9 +6,6 @@ require "sass"
 require "yaml"
 require "./lib/facet_controller"
 
-Configuration = YAML.load_file("./lib/config.yaml")
-Logfile = Configuration["logfile"]
-FacetController.connect_db(Configuration["db_path"])
 
 def logging(query)
   log = #{query} + "\t" + Time.now.to_s
@@ -19,23 +16,34 @@ def query_filter(query_raw)
   query_raw
 end
 
+def total_count
+  db = FacetController.new
+  total = db.size
+  #db.close
+  total
+end
+
 def soy_filter(condition)
   db = FacetController.new
   
   tax = condition[:taxonid]
   stu = condition[:study_type]
-  ins = condition[:instrument])
+  ins = condition[:instrument]
 
-  { taxonid: db.count_taxon_id(tax),
-    study_id: db.count_study_type(stu),
-    instrument: db.count_instrument(ins),
-    on_demand: db.count_on_demand(tax, stu, ins) }
+  filtered = { taxonid: db.count_taxon_id(tax),
+               study_type: db.count_study_type(stu),
+               instrument: db.count_instrument(ins),
+               on_demand: db.count_on_demand(tax, stu, ins) }
+  #db.close
+  filtered
 end
 
 def soy_search(query)
   if query
     db = FacetController.new
-    db.search_fulltext(query)
+    result = db.search_fulltext(query)
+    #db.close
+    result
   end
 end
 
@@ -47,8 +55,17 @@ end
 
 set :haml, :format => :html5
 
+configure do
+  Configuration = YAML.load_file("./lib/config.yaml")
+  Logfile = Configuration["logfile"]
+end
+
 before do
    logging(query) if params[:query]
+end
+
+get "/style.css" do
+  sass :style
 end
 
 get "/" do
@@ -56,22 +73,39 @@ get "/" do
 end
 
 post "/filter" do
+  db_path = Configuration["db_path"]
+  FacetController.connect_db(db_path)
+
   @sp = params[:species]
   @st = params[:study_type]
   @pl = params[:platform]
   
-  taxonid = sp
-  study_type = st
-  instrument = pl
+  @total_number = total_count
+
+  taxonid = @sp
+  study_type = @st
+  instrument = @pl
   condition = { taxonid: taxonid, study_type: study_type, instrument: instrument }
   @number_of_records = soy_filter(condition)
+  
+  db = FacetController.new
+  db.close
+
   haml :filter
 end
 
 post "/search" do
-  query_raw = params[:query]
-  query = query_filter(query_raw)
-  @result = soy_search(query)
+  db_path = Configuration["db_path"]
+  FacetController.connect_db(db_path)
+
+  query_raw = params[:search_query]
+  @query = query_filter(query_raw)
+  @result = soy_search(@query)
+  puts @result
+
+  db = FacetController.new
+  db.close
+
   if @result
     haml :search
   else
