dc7ff194e917f7394b7c30b05fe09dc943c0fd57
tazro inutano ohta <inutano@gmail.com>
2012-12-28 17:31:03 +0900

    experiment section done. parser object will be generated on each method

--- lib/project_report.rb
+++ lib/project_report.rb
@@ -12,17 +12,14 @@ class ProjectReport
     @studyid = studyid
     pgen = SRAParserGen.new(studyid)
     @sub_parser = pgen.submission_parser
-    @study_parser = pgen.study_parser
-    @exp_parser = pgen.experiment_parser
     @sample_parser = pgen.sample_parser
     @run_parser = pgen.run_parser
-    @pub_parser = pgen.pubmed_parser
-    @pmc_parser = pgen.pmc_parser
   end
   
   def project_info
-    if @study_parser.size == 1
-      sp = @study_parser.first
+    study_parser = pgen.study_parser
+    if study_parser.size == 1
+      sp = study_parser.first
       { study_title: sp.study_title,
         study_abstract: sp.study_abstract,
         study_description: sp.study_description,
@@ -33,7 +30,8 @@ class ProjectReport
   end
   
   def paper_info
-    @pub_parser.select{|n| n }.map do |pp|
+    pub_parser = pgen.pubmed_parser
+    pub_parser.select{|n| n }.map do |pp|
       { pmid: pp.pmid,
         journal: pp.journal_title,
         title: pp.article_title,
@@ -45,7 +43,8 @@ class ProjectReport
   end
   
   def pmc_info
-    @pmc_parser.select{|n| n }.map do |pp|
+    pmc_parser = pgen.pmc_parser
+    pmc_parser.select{|n| n }.map do |pp|
       { pmcid: pp.pmcid,
         journal_title: pp.journal_title,
         introduction: pp.body.select{|s| s[:sec_title] =~ /introduction/i },
@@ -57,8 +56,21 @@ class ProjectReport
     end
   end
   
-  def before_seq
-    @sample_parser.
+  def experiment_info
+    exp_parser = pgen.experiment_parser
+    exp_parser.select{|n| n }.map do |ep|
+      { exp_title: ep.title,
+        design_description: ep.design_description,
+        sampleid: ep.sample_accession,
+        lib_name: ep.library_name,
+        lib_strategy: ep.library_strategy,
+        lib_source: ep.library_source,
+        lib_selection: ep.library_selection,
+        lib_layout: ep.library_layout,
+        lib_protocol: ep.library_construction_protocol,
+        platform: ep.platform,
+        instrument: ep.instrument_model }
+    end
   end
   
   def sequencing
