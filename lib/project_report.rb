23573abb458a1d3acceb8784d09683091630b462
tazro inutano ohta <inutano@gmail.com>
2013-01-29 17:05:24 +0900

    fixed a lot of bugs

--- lib/project_report.rb
+++ lib/project_report.rb
@@ -2,6 +2,7 @@
 
 require "yaml"
 require "open-uri"
+require "json"
 require File.expand_path(File.dirname(__FILE__)) + "/sra_metadata_parser"
 require File.expand_path(File.dirname(__FILE__)) + "/pubmed_metadata_parser"
 require File.expand_path(File.dirname(__FILE__)) + "/pmc_metadata_parser"
@@ -35,7 +36,7 @@ class ProjectReport
   attr_reader :subid
   
   def study
-    prsr = SRAMetadataParser::Study(@studyid, @xml_head + ".study.xml")
+    prsr = SRAMetadataParser::Study.new(@studyid, @xml_head + ".study.xml")
     { study_title: prsr.study_title,
       study_type: prsr.study_type }
   end
@@ -43,7 +44,7 @@ class ProjectReport
   def sample
     sampleid_array = `grep #{@studyid} #{@@run_members} | cut -f 4`.split("\n")
     sample_table = sampleid_array.uniq.map do |sampleid|
-      prsr = SRAMetadataParser::Sample(sampleid, @xmlhead + ".sample.xml")
+      prsr = SRAMetadataParser::Sample.new(sampleid, @xml_head + ".sample.xml")
       if prsr
         taxonid = prsr.taxon_id
         field = `cat #{@@taxon_table} | awk -F "|" '$1 == #{taxonid} && $4 ~ "scientific" { print $2 }'`
@@ -60,7 +61,7 @@ class ProjectReport
   def experiment
     expid_array = `grep #{@studyid} #{@@run_members} | cut -f 3`.split("\n")
     exp_table = expid_array.uniq.map do |expid|
-      prsr = SRAMetadataParser::Experiment(expid, @xmlhead + ".experiment.xml")
+      prsr = SRAMetadataParser::Experiment.new(expid, @xml_head + ".experiment.xml")
       if prsr
         { expid: expid,
           lib_layout: prsr.library_layout,
@@ -76,7 +77,7 @@ class ProjectReport
     Dir.entries(path).select{|f| f =~ /#{runid}/ }.map do |read|
       data_path = File.join(path, read, "fastqc_data.txt")
       prsr = FastQCParser.new(data_path)
-      { read: read.sub(/_fastqc$/,"")
+      { read: read.sub(/_fastqc$/,""),
         total_seq: prsr.total_sequences,
         seq_length: prsr.sequence_length }
     end
@@ -106,8 +107,8 @@ class ProjectReport
       expid = res[1]
       sampleid = res[2]
       
-      sample = self.sample.select{|n| n[:sampleid] == sampleid }
-      experiment = self.experiment.select{|n| n[:expid] == expid }
+      sample = self.sample.select{|n| n[:sampleid] == sampleid }.first
+      experiment = self.experiment.select{|n| n[:expid] == expid }.first
       
       study_type = self.study[:study_type]
       organism = sample[:scientific_name]
@@ -138,7 +139,7 @@ class ProjectReport
     end
     
     sample_runcount.keys.map do |sampleid|
-      sample = self.sample.select{|n| n[:sampleid] == sampleid }
+      sample = self.sample.select{|n| n[:sampleid] == sampleid }.first
       sample_description = sample[:sample_description]
       { sampleid: sampleid,
         sample_description: sample_description,
@@ -202,98 +203,8 @@ end
 if __FILE__ == $0
   require "ap"
   ProjectReport.load_files("./config.yaml")
-  id = "DRP000001"
+  id = "DRP000017" #"DRP000001"
+  
   pr = ProjectReport.new(id)
   ap pr.report
 end
-
-
-=begin
-class ProjectReport
-  def self.load_files(config_path)
-    SRAParserGen.load_files(config_path)
-  end
-  
-  def initialize(studyid)
-    @studyid = studyid
-    @pgen = SRAParserGen.new(studyid)
-  end
-  
-  def project_info
-    study_parser = @pgen.study_parser
-    if study_parser.size == 1
-      sp = study_parser.first
-      { study_title: sp.study_title,
-        study_type: sp.study_type }
-    end
-  end
-  
-  def paper_info
-    pub_parser = @pgen.pubmed_parser
-    if pub_parser
-      pub_parser.select{|n| n }.map do |pp|
-        { pmid: pp.pmid,
-          journal: pp.journal_title,
-          title: pp.article_title,
-          abstract: pp.abstract,
-          affiliation: pp.affiliation,
-          authors: pp.authors.map{|a| a.values.join("\s")},
-          date: pp.date_created.values.join("/") }
-      end
-    end
-  end
-  
-  def pmc_info
-    pmc_parser = @pgen.pmc_parser
-    if pmc_parser
-      pmc_parser.select{|n| n }.map do |pp|
-        body = pp.body.select{|sec| sec }
-        { pmcid: pp.pmcid,
-          journal_title: pp.journal_title,
-          introduction: body.select{|s| s[:sec_title] =~ /introduction/i or s[:sec_title] =~ /background/i },
-          methods: body.select{|s| s[:sec_title] =~ /method/i },
-          results: body.select{|s| s[:sec_title] =~ /result/i },
-          discussion: body.select{|s| s[:sec_title] =~ /discussion/i },
-          references: pp.ref_journal_list,
-        }
-      end
-    end
-  end
-  
-  def experiment_info
-    exp_parser = @pgen.experiment_parser
-    exp_parser.select{|n| n }.map do |ep|
-      { sampleid: ep.sample_accession,
-        lib_layout: ep.library_layout,
-        platform: ep.platform,
-        lib_protocol: ep.library_construction_protocol,
-        instrument: ep.instrument_model }
-    end
-  end
-  
-  def sample_info
-    sample_parser = @pgen.sample_parser
-    sample_parser.select{|n| n }.map do |sp|
-      { taxonid: sp.taxon_id,
-        common_name: sp.common_name,
-        scientific_name: sp.scientific_name }
-    end
-  end
-  
-  def run_info
-    run_parser = @pgen.run_parser
-    run_parser.select{|n| n }.map do |rp|
-      # nothing
-    end
-  end
-  
-  def report
-    { project: self.project_info,
-      paper: self.paper_info,
-      pmc: self.pmc_info,
-      experiment: self.experiment_info,
-      sample: self.sample_info }
-  end
-end
-
-=end
