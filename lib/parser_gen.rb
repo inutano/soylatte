5eb54e3a2dd5b3065b4b4f54f8fde7f4babce042
tazro inutano ohta <inutano@gmail.com>
2013-02-01 17:43:07 +0900

    fully fixed as it works with groonga id table db

--- lib/parser_gen.rb
+++ lib/parser_gen.rb
@@ -3,6 +3,7 @@
 require "yaml"
 require "open-uri"
 require "json"
+require "groonga"
 require "#{File.expand_path(File.dirname(__FILE__))}/sra_metadata_parser"
 require "#{File.expand_path(File.dirname(__FILE__))}/pubmed_metadata_parser"
 require "#{File.expand_path(File.dirname(__FILE__))}/pmc_metadata_parser"
@@ -19,21 +20,28 @@ class SRAParserGen
     publication_url = file_path["publication"]
     publication_raw = open(publication_url).read
     publication_json = JSON.parse(publication_raw, :symbolize_names => true)
-    @@publication = {}    
+    @@publication = {}
     publication_json[:ResultSet][:Result].each do |node|
       @@publication[node[:sra_id].intern] ||= []
       @@publication[node[:sra_id].intern] << node[:pmid]
     end
   end
   
-  def initialize(id)
+  def initialize(id, db)
     @id = id
+    @db = db
     @id_type = id.slice(2,1)
     @subid = case @id_type
              when "A"
                id
-             else
-               `grep -m 1 #{id} #{@@accessions} | cut -f 2`.chomp
+             when "P"
+               @db.select{|r| r.project == id }.first.submission
+             when "X"
+               @db.select{|r| r.experiment == id }.first.submission
+             when "S"
+               @db.select{|r| r.sample == id }.first.submission
+             when "R"
+               @db.select{|r| r.key.key == id }.first.submission
              end
     @xml_head = File.join(@@xmlbase, @subid.slice(0,6), @subid)
   end
@@ -41,83 +49,93 @@ class SRAParserGen
   
   def submission_parser
     xml = File.join(@xml_head, "#{@subid}.submission.xml")
-    [SRAMetadataParser::Submission.new(@subid, xml)]
-  rescue Errno::ENOENT, NameError
-    nil
+    if File.exist?(xml)
+      [SRAMetadataParser::Submission.new(@subid, xml)]
+    end
   end
   
   def study_parser
     studyid_arr = case @id_type
-              when "P"
-                [@id]
-              when "A"
-                `grep #{@id} #{@@accessions} | grep "^.RP" | cut -f 1`.split("\n")
-              when "R"
-                `grep #{@id} #{@@run_members} | cut -f 5 | sort -u`.split("\n")
-              else
-                `grep #{@id} #{@@accessions} | cut -f 13 | sort -u`.split("\n")
-              end
+                  when "P"
+                    [@id]
+                  when "A"
+                    @db.select{|r| r.submission == id }.map{|r| r.project }.uniq
+                  when "X"
+                    @db.select{|r| r.experiment == id }.map{|r| r.project }.uniq
+                  when "S"
+                    @db.select{|r| r.sample == id }.map{|r| r.project }.uniq
+                  when "R"
+                    @db.select{|r| r.key.key == id }.map{|r| r.project }.uniq
+                  end
     xml = File.join(@xml_head, "#{@subid}.study.xml")
-    studyid_arr.select{|id| id =~ /^(S|E|D)RP\d{6}$/ }.map do |studyid|
-      SRAMetadataParser::Study.new(studyid, xml)
+    studyid_arr.map do |studyid|
+      if File.exist?(xml)
+        SRAMetadataParser::Study.new(studyid, xml)
+      end
     end
-  rescue Errno::ENOENT, NameError
-    nil
   end
   
   def experiment_parser
     expid_arr = case @id_type
-              when "X"
-                [@id]
-              when "A"
-                `grep #{@id} #{@@accessions} | grep "^.RX" | cut -f 1`.split("\n")
-              when "R"
-                `grep #{@id} #{@@run_members} | cut -f 3 | sort -u`.split("\n")
-              else
-                `grep #{@id} #{@@accessions} | cut -f 11 | sort -u`.split("\n")
-              end
+                when "X"
+                  [@id]
+                when "A"
+                  @db.select{|r| r.submission == id }.map{|r| r.experiment }.uniq
+                when "P"
+                  @db.select{|r| r.project == id }.map{|r| r.experiment }.uniq
+                when "S"
+                  @db.select{|r| r.sample == id }.map{|r| r.experiment }.uniq
+                when "R"
+                  @db.select{|r| r.key.key == id }.map{|r| r.experiment }.uniq
+                end
     xml = File.join(@xml_head, "#{@subid}.experiment.xml")
-    expid_arr.select{|id| id =~ /^(S|E|D)RX\d{6}$/ }.map do |expid|
-      SRAMetadataParser::Experiment.new(expid, xml)
+    expid_arr.map do |expid|
+      if File.exist?(xml)
+        SRAMetadataParser::Experiment.new(expid, xml)
+      end
     end
-  rescue Errno::ENOENT, NameError
-    nil
   end
   
   def sample_parser
     sampleid_arr = case @id_type
-              when "S"
-                [@id]
-              when "A"
-                `grep #{@id} #{@@accessions} | grep "^.RS" | cut -f 1`.split("\n")
-              when "R"
-                `grep #{@id} #{@@run_members} | cut -f 4 | sort -u`.split("\n")
-              else
-                `grep #{@id} #{@@accessions} | cut -f 12 | sort -u`.split("\n")
-              end
+                   when "S"
+                     [@id]
+                   when "A"
+                     @db.select{|r| r.submission == id }.map{|r| r.sample }.uniq
+                   when "P"
+                     @db.select{|r| r.project == id }.map{|r| r.sample }.uniq
+                   when "X"
+                     @db.select{|r| r.experiment == id }.map{|r| r.sample }.uniq
+                   when "R"
+                     @db.select{|r| r.key.key == id }.map{|r| r.sample }.uniq
+                   end
     xml = File.join(@xml_head, "#{@subid}.sample.xml")
-    sampleid_arr.select{|id| id =~ /^(S|E|D)RS\d{6}$/ }.map do |sampleid|
-      SRAMetadataParser::Sample.new(sampleid, xml)
+    sampleid_arr.map{|ids| ids.split(",") }.flatten.uniq.map do |sampleid|
+      if File.exist?(xml)
+        SRAMetadataParser::Sample.new(sampleid, xml)
+      end
     end
-  rescue Errno::ENOENT, NameError
-    nil
   end
   
   def run_parser
     runid_arr = case @id_type
-              when "R"
-                [@id]
-              when "A"
-                `grep #{@id} #{@@accessions} | grep "^.RR" | cut -f 1 | sort -u`.split("\n")
-              else
-                `grep #{@id} #{@@run_members} | cut -f 1 | sort -u`.split("\n")
-              end
+                when "S"
+                  [@id]
+                when "A"
+                  @db.select{|r| r.submission == id }.map{|r| r.key.key }.uniq
+                when "P"
+                  @db.select{|r| r.project == id }.map{|r| r.key.key }.uniq
+                when "X"
+                  @db.select{|r| r.experiment == id }.map{|r| r.key.key }.uniq
+                when "S"
+                  @db.select{|r| r.sample == id }.map{|r| r.key.key }.uniq
+                end
     xml = File.join(@xml_head, "#{@subid}.run.xml")
-    runid_arr.select{|id| id =~ /^(S|E|D)RR\d{6}$/ }.map do |runid|
-      SRAMetadataParser::Run.new(runid, xml)
+    runid_arr.map do |runid|
+      if File.exist?(xml)
+        SRAMetadataParser::Run.new(runid, xml)
+      end
     end
-  rescue Errno::ENOENT, NameError
-    nil
   end
   
   def pubmed_parser
@@ -157,7 +175,7 @@ if __FILE__ == $0
   ap "loadfiles"
   ap t00 - t0
   
-  ids = ["DRP000017"] #["DRP000001","DRP000017","DRR000030"]
+  ids = ["DRP000001","DRP000017","DRR000030"]
   ids.each do |id|
     
     
