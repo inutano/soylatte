0b1edefbbee33ed5ce0871c23bb041690e8f772b
tazro inutano ohta <inutano@gmail.com>
2013-02-04 18:57:23 +0900

    fully refactorred project_report and now is faster. I do not wanna make it even better anymore.

--- lib/project_report.rb
+++ lib/project_report.rb
@@ -1,256 +1,224 @@
 # -*- coding: utf-8 -*-
 
 require "yaml"
-require "open-uri"
+require "groonga"
 require "json"
+require "open-uri"
+require "parallel"
+
 require File.expand_path(File.dirname(__FILE__)) + "/sra_metadata_parser"
 require File.expand_path(File.dirname(__FILE__)) + "/pubmed_metadata_parser"
 require File.expand_path(File.dirname(__FILE__)) + "/pmc_metadata_parser"
 require File.expand_path(File.dirname(__FILE__)) + "/fastqc_result_parser"
 
 class ProjectReport
-  def self.load_files(config_path)
-    config = YAML.load_file(config_path)
-    fpath = config["file_path"]
-    @@xmlbase = fpath["xmlbase"]
-    @@accessions = fpath["sra_accessions"]
-    @@run_members = fpath["sra_run_members"]
-    @@taxon_table = fpath["taxon_table"]
-    @@fqc_path = config["fqc_path"]
-
-    @@eutil_base = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?"
-
-    raw_json = open(fpath["publication"]).read
-    @@paper_json = JSON.parse(raw_json, :symbolize_names => true)
-
-    @@pmcid_table = fpath["PMC-ids"]
-  end
+  config_path = "/Users/inutano/project/soylatte/lib/config.yaml"
+  @@db_path = YAML.load_file(config_path)["idtable_path"]
   
-  def initialize(studyid)
+  def initialize(studyid, config_path)
     @studyid = studyid
-    @subid = `grep "^#{@studyid}" #{@@accessions} | cut -f 2`.chomp
-    @xml_head = File.join(@@xmlbase, @subid.slice(0..5), @subid, @subid)
+    @config = YAML.load_file(config_path)
+    self.connect_db
+    self.get_records
 
-    @paperinfo = @@paper_json[:ResultSet][:Result].select{|ent| ent[:sra_id] == @subid }
+    subid_a = @records.map{|r| r.submission }.uniq
+    if subid_a.size > 1
+      raise ArgumentError
+    else
+      subid = subid_a.first
+      xmlbase = @config["file_path"]["xmlbase"]
+      @xml = File.join(xmlbase, subid.slice(0..5), subid, subid)
+    end
+    
+    raw_json = open(@config["file_path"]["publication"]).read
+    json = JSON.parse(raw_json, :symbolize_names => true)
+    @paper = json[:ResultSet][:Result].select{|ent| ent[:sra_id] == subid }
+    @pmcid_table = @config["file_path"]["PMC-ids"]
+    @eutil_base = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?"
   end
-  attr_reader :subid
+  attr_reader :iddb
   
-  def study
-    prsr = SRAMetadataParser::Study.new(@studyid, @xml_head + ".study.xml")
-    { studyid: @studyid,
-      study_title: prsr.study_title,
-      study_type: prsr.study_type }
+  def connect_db
+    if !@iddb || @iddb.closed?
+      @iddb = Groonga::Database.open(@@db_path)
+      @db ||= Groonga["IDtable"]
+    end
   end
   
-  def sample
-    sampleid_array = `grep #{@studyid} #{@@run_members} | cut -f 4`.split("\n")
-    sample_table = sampleid_array.uniq.map do |sampleid|
-      prsr = SRAMetadataParser::Sample.new(sampleid, @xml_head + ".sample.xml")
-      if prsr
-        taxonid = prsr.taxon_id
-        field = `cat #{@@taxon_table} | awk -F "|" '$1 == #{taxonid} && $4 ~ "scientific" { print $2 }'`
-        scientific_name = field.chomp.delete("\t")
-        { sampleid: sampleid,
-          sample_description: prsr.sample_description,
-          taxonid: taxonid,
-          scientific_name: scientific_name }
+  def get_records
+    @records ||= @db.select{|r| r.project == @studyid }
+  end
+  
+  def study_info
+    study_parser = SRAMetadataParser::Study.new(@studyid, @xml + ".study.xml")
+    { study_title: study_parser.study_title,
+      study_type: study_parser.study_type }
+  end
+  
+  def experiment_info
+    hash = {}
+    exp_xml = @xml + ".experiment.xml"
+    @records.map{|r| r.experiment }.uniq.each do |expid|
+      exp_parser = SRAMetadataParser::Experiment.new(expid, exp_xml)
+      if exp_parser
+        hash[expid] = { lib_layout: exp_parser.library_layout,
+                        instrument: exp_parser.instrument_model }
       end
     end
-    sample_table.compact
+    hash
   end
   
-  def experiment
-    expid_array = `grep #{@studyid} #{@@run_members} | cut -f 3`.split("\n")
-    exp_table = expid_array.uniq.map do |expid|
-      prsr = SRAMetadataParser::Experiment.new(expid, @xml_head + ".experiment.xml")
-      if prsr
-        { expid: expid,
-          lib_layout: prsr.library_layout,
-          platform: prsr.platform,
-          instrument: prsr.instrument_model }
+  def sample_info
+    hash = {}
+    sample_xml = @xml + ".sample.xml"
+    @records.map{|r| r.sample }.uniq.each do |sampleid|
+      sample_parser = SRAMetadataParser::Sample.new(sampleid, sample_xml)
+      if sample_parser
+        taxonid = sample_parser.taxon_id
+        hash[sampleid] = { sample_description: sample_parser.sample_description,
+                           taxonid: taxonid,
+                           scientific_name: self.taxonid2sname(taxonid) }
       end
     end
-    exp_table.compact
+    hash
   end
   
-  def read_profile(runid)
-    path = File.join(@@fqc_path, runid.slice(0..5), runid)
+  def taxonid2sname(taxonid)
+    taxon_table = @config["file_path"]["taxon_table"]
+    field = `cat #{taxon_table} | awk -F "|" '$1 == #{taxonid} && $4 ~ "scientific" { print $2 }'`
+    field.chomp.delete("\t")
+  end
+  
+  def read_info(runid)
+    path = File.join(@config["fqc_path"], runid.slice(0..5), runid)
     Dir.entries(path).select{|f| f =~ /#{runid}/ }.map do |read|
       data_path = File.join(path, read, "fastqc_data.txt")
-      prsr = FastQCParser.new(data_path)
+      qc_parser = FastQCParser.new(data_path)
       { read: read.sub(/_fastqc$/,""),
-        total_seq: prsr.total_sequences,
-        seq_length: prsr.sequence_length }
+        total_seq: qc_parser.total_sequences,
+        seq_length: qc_parser.sequence_length }
     end
   rescue Errno::ENOENT
     nil
   end
   
-  def general_table
-    study = self.study
-    sample = self.sample
-    experiment = self.experiment
-    
-    studyid = study[:studyid]
-    study_title = study[:study_title]
-    study_type = study[:study_type]
-    scientific_name = sample.map{|n| n[:scientific_name] }.uniq.join(", ")
-    instrument = experiment.map{|n| n[:instrument] }.uniq.join(", ")
-    
-    { studyid: studyid,
-      study_title: study_title,
-      study_type: study_type,
-      scientific_name: scientific_name,
-      instrument: instrument }
-  end
-  
-  def run_table
-    line = `grep #{@studyid} #{@@run_members} | cut -f 1,3,4`.split("\n")
-    run_exp_sample = line.map{|line| line.split("\t") }
-    run_table = run_exp_sample.map do |res|
-      runid = res[0]
-      expid = res[1]
-      sampleid = res[2]
-      
-      sample = self.sample.select{|n| n[:sampleid] == sampleid }.first
-      experiment = self.experiment.select{|n| n[:expid] == expid }.first
-      
-      study_type = self.study[:study_type]
-      organism = sample[:scientific_name]
-      instrument = experiment[:instrument]
-      lib_layout = experiment[:lib_layout]
-      
-      { runid: runid,
-        expid: expid,
-        sampleid: sampleid,
-        study_type: study_type,
-        organism: organism,
-        instrument: instrument,
-        lib_layout: lib_layout,
-        read_profile: self.read_profile(runid) }
-    end
-  end
-  
-  def sample_table
-    line = `grep #{@studyid} #{@@run_members} | cut -f 1,4`.split("\n")
-    run_sample = line.map{|line| line.split("\t") }
-    
-    sample_runcount = {}
-    run_sample.each do |rs|
-      runid = rs[0]
-      sampleid = rs[1]
-      sample_runcount[sampleid] ||= []
-      sample_runcount[sampleid].push(runid)
-    end
-    
-    sample_runcount.keys.map do |sampleid|
-      sample = self.sample.select{|n| n[:sampleid] == sampleid }.first
-      sample_description = sample[:sample_description]
-      { sampleid: sampleid,
-        sample_description: sample_description,
-        runid: sample_runcount[sampleid] }
-    end
-  end
-  
   def paper
-    if @paperinfo
-      @paperinfo.map do |entry|
+    if @paper
+      @paper.map do |entry|
         pmid = entry[:pmid]
         arg = "db=pubmed&id=#{pmid}&retmode=xml"
-        pm_prsr = PubMedMetadataParser.new(open(@@eutil_base + arg).read)
-        
-        pmcid = `grep -m 1 #{pmid} #{@@pmcid_table}`.split(",")[8]
-        pmc = if pmcid
-                arg = "db=pmc&id=#{pmcid}&retmode=xml"
-                pmc_prsr = PMCMetadataParser.new(open(@@eutil_base + arg).read)
-                body = pmc_prsr.body.compact
-                introduction = body.select{|s| s[:sec_title] =~ /introduction|background/i }
-                methods = body.select{|s| s[:sec_title] =~ /methods/i }
-                results = body.select{|s| s[:sec_title] =~ /results/i }
-                discussion = body.select{|s| s[:sec_title] =~ /discussion/i }
-                
-                { pmcid: pmcid,
-                  introduction: introduction,
-                  methods: methods,
-                  results: results,
-                  discussion: discussion,
-                  references: pmc_prsr.ref_journal_list,
-                  cited_by: pmc_prsr.cited_by }
-              end
-        
+        pm_parser = PubMedMetadataParser.new(open(@eutil_base + arg).read)
+        pmcid = pm_parser.pmcid
         { pmid: pmid,
-          journal: pm_prsr.journal_title,
-          title: pm_prsr.article_title,
-          abstract: pm_prsr.abstract,
-          affiliation: pm_prsr.affiliation,
-          authors: pm_prsr.authors.map{|a| a.values.join("\s") },
-          date: pm_prsr.date_created.values.join("/"),
-          pmc: pmc }
+          journal: pm_parser.journal_title,
+          title: pm_parser.article_title,
+          abstract: pm_parser.abstract,
+          affiliation: pm_parser.affiliation,
+          authors: pm_parser.authors.map{|a| a.values.join("\s") },
+          date: pm_parser.date_created.values.join("/"),
+          pmc: self.pmc(pmcid)}
       end
     end
   end
   
-  def pmc
-    if @paperinfo
-      pmcinfo = @paperinfo.map do |entry|
-        pmid = entry[:pmid]
-        pmcid = `grep -m 1 #{pmid} #{@@pmcid_table}`.split(",")[8]
-        if pmcid
-          arg = "db=pmc&id=#{pmcid}&retmode=xml"
-          prsr = PMCMetadataParser.new(open(@@eutil_base + arg).read)
-          body = prsr.body.compact
-          introduction = body.select{|s| s[:sec_title] =~ /introduction|background/i }
-          methods = body.select{|s| s[:sec_title] =~ /methods/i }
-          results = body.select{|s| s[:sec_title] =~ /results/i }
-          discussion = body.select{|s| s[:sec_title] =~ /discussion/i }
-          
-          { pmcid: pmcid,
-            introduction: introduction,
-            methods: methods,
-            results: results,
-            discussion: discussion,
-            references: prsr.ref_journal_list,
-            cited_by: prsr.cited_by }
-        end
-      end
-      pmcinfo.compact if !pmcinfo.compact.empty?
+  def pmc(pmcid)
+    if pmcid
+      arg = "db=pmc&id=#{pmcid}&retmode=xml"
+      pmc_parser = PMCMetadataParser.new(open(@eutil_base + arg).read)
+      body = pmc_parser.body.compact
+      methods = body.select{|s| s[:sec_title] =~ /methods/i }
+      results = body.select{|s| s[:sec_title] =~ /results/i }
+      { pmcid: pmcid,
+        methods: methods,
+        results: results,
+        reference: pmc_parser.ref_journal_list,
+        cited_by: pmc_parser.cited_by }
     end
   end
   
   def report
-    { general: self.general_table,
+    study_info = self.study_info
+    experiment_info = self.experiment_info
+    sample_info = self.sample_info
+    
+    general = { studyid: @studyid,
+                study_title: study_info[:study_title],
+                study_type: study_info[:study_type],
+                scientific_name: sample_info.values.map{|n| n[:scientific_name] }.uniq.join(", "),
+                instrument: experiment_info.values.map{|n| n[:instrument]}.uniq.join(",") }
+    
+    runids = @records.map{|r| r.key.key }
+    run_table = Parallel.map(runids) do |runid|
+      rec = @db[runid]
+      expid = rec.experiment
+      sampleid = rec.sample
+      
+      if experiment_info.has_key?(expid)
+        instrument = experiment_info[expid][:instrument]
+        lib_layout = experiment_info[expid][:lib_layout]
+      else
+        exp_xml = @xml + ".experiment.xml"
+        exp_parser = SRAMetadataParser::Experiment.new(expid, exp_xml)
+        if exp_parser
+          instrument = exp_parser.instrument_model
+          lib_layout = exp_parser.library_layout
+        else
+          insturment = "no data"
+          lib_layout = "no data"
+        end
+      end
+      
+      if sample_info.has_key?(sampleid)
+        scientific_name = sample_info[sampleid][:scientific_name]
+      else
+        sample_xml = @xml + ".sample.xml"
+        sample_parser = SRAMetadataParser::Sample.new(sampleid, sample_xml)
+        if sample_parser
+          taxonid = sample_parser.taxon_id
+          scientific_name = taxonid2sname(taxonid)
+        else
+          scientific_name = "no data"
+        end
+      end
+      
+      { runid: runid,
+        expid: expid,
+        sampleid: sampleid,
+        study_type: study_info[:study_type],
+        instrument: instrument,
+        lib_layout: lib_layout,
+        organism: scientific_name,
+        read_profile: self.read_info(runid) }
+    end
+    
+    sample_runcount = {}
+    sample_run = run_table.each do |n|
+      runid = n[:runid]
+      sampleid = n[:sampleid]
+      sample_runcount[sampleid] ||= []
+      sample_runcount[sampleid].push(runid)
+    end
+  
+    sample_table = sample_runcount.keys.map do |sampleid|
+      { sampleid: sampleid,
+        sample_description: sample_info[sampleid][:sample_description],
+        runid: sample_runcount[sampleid] }
+    end
+    
+    { general: general,
       paper: self.paper,
-      pmc: self.pmc,
-      run_table: self.run_table,
-      sample_table: self.sample_table }
+      run_table: run_table,
+      sample_table: sample_table }
   end
-end
-
-def mess(message)
-  puts message + "\t" + Time.now.strftime("%H:%M:%S")
 end
 
 if __FILE__ == $0
   require "ap"
-
-  ProjectReport.load_files("./config.yaml")  
-  id = "DRP000169" #"DRP000017" #"DRP000001"
-  
-  mess "creating ProjectReport object"
-  pr = ProjectReport.new(id)
-  
-  mess "general table"
-  ap pr.general_table
-
-  mess "paper info"
-  ap pr.paper
-
-  mess "pmc info"
-  ap pr.pmc
-  
-  mess "run table"
-  ap pr.run_table
-
-  mess "sample table"
-  ap pr.sample_table
+  ap Time.now
+  config_path = "./config.yaml"
+  ids = ["DRP000169", "DRP000017", "DRP000001"]
+  ids.each do |id|
+    ap ProjectReport.new(id, config_path).report.class
+    ap Time.now
+  end
 end
