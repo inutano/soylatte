917b8b6e9735bdb5423b7eed736618b824ea1fd3
tazro inutano ohta <inutano@gmail.com>
2013-02-05 15:15:25 +0900

    changed to devide DB duplication check and insert configuration

--- lib/db_update.rb
+++ lib/db_update.rb
@@ -59,7 +59,7 @@ end
 if __FILE__ == $0
   config_path = "../config.yaml"
   config = YAML.load_file(config_path)
-  #db_path = config["db_path"]
+  #db_path = config["project_db_path"]
   db_path = "../db_test/project.db"
 
   Groonga::Context.default_options = { encoding: :utf8 }
@@ -70,23 +70,23 @@ if __FILE__ == $0
   
   when "--update"
     accessions = config["file_path"]["sra_accessions"]
-    studyids = `grep '^.RP' #{accessions} | grep 'live' | grep -v 'control' | cut -f 1 | sort -u`.split("\n")[0..999]
-    
-    Groonga::Database.open(db_path)
-    MetadataParser.load_files(config_path)
-    
-    ap Groonga["Projects"]
+    studyids = `grep '^.RP' #{accessions} | grep 'live' | grep -v 'control' | cut -f 1 | sort -u`.split("\n")[0..99]
 
-    Groonga::Database.open("../db/idtable.db")
-    iddb = Groonga["IDtable"]
-    
-    ap Groonga["IDtable"]
-    
-    inserts = Parallel.map(studyids) do |studyid|
+    prj_db = Groonga::Database.open(db_path)
+    not_recorded = Parallel.map(studyids) do |studyid|
       if !Groonga["Projects"][studyid]
-        f = MetadataParser.new(studyid, iddb)
-        f.insert
+        studyid
       end
+    end
+    
+    MetadataParser.load_files(config_path)
+    inserts = Parallel.map(not_recorded.compact) do |studyid|
+      f = MetadataParser.new(studyid)
+      f.insert
+    end
+    
+    if prj_db.closed?
+      prj_db = Groonga::Database.open(db_path)
     end
     
     Parallel.each(inserts) do |insert|
@@ -98,7 +98,7 @@ if __FILE__ == $0
     db = Groonga["Projects"]
     ap db.size
     if ARGV[1]
-      ap db.select{|r| r.fulltext =~ /#{ARGV[1]}/ }.map{|r| r.key.key }
+      ap db.select{|r| r.fulltext =~ ARGV[1] }.map{|r| r.key.key }
     end
   end
 end
