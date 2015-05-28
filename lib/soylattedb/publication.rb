f60ba9054d3461503f2a14371e7dbb5c5835cbbf
tazro inutano ohta <inutano@gmail.com>
2015-05-28 11:50:32 +0900

    ready to implement

--- lib/soylattedb/publication.rb
+++ lib/soylattedb/publication.rb
@@ -5,7 +5,15 @@ require 'groonga'
 class SoylatteDB
   class Publication
     class << self
-      def load
+      def load(db, sub_id_list)
+        load_pubmed(db, sub_id_list)
+        load_pmc(db, sub_id_list)
+      end
+
+      def load_pubmed(db, sub_id_list)
+      end
+
+      def load_pmc(db, sub_id_list)
       end
 
       def get_publication_xml
@@ -19,3 +27,34 @@ class SoylatteDB
     end
   end
 end
+
+=begin
+    # hash for { pmid => [studyid1, studyid2, ..] } or { pmcid => [studyid1, studyid2, ...] }
+    def bulk_description(hash, projects)
+      num_of_parallel = 100
+      while !hash.empty?
+        pubid_studyids = Hash.new([])
+        id_list = []
+        times_loop = num_of_parallel > hash.size ? hash.size : num_of_parallel
+        times_loop.times do
+          item = hash.shift
+          pubid = item[0]
+          studyids = item[1]
+
+          id_list << item[0]
+          pubid_studyids[pubid] = studyids
+        end
+        DBupdate.new(id_list).bulk_retrieve.each_pair do |id, text|
+          studyids = pubid_studyids[id]
+          studyids.each do |studyid|
+            record = projects[studyid]
+            exists = record[:search_fulltext]
+            record[:search_fulltext] = [exists, text].join("\s")
+          end
+        end
+      end
+    end
+    bulk_description(pmid_hash, projects)
+    bulk_description(pmcid_hash, projects)
+
+=end
