af4959b5055ff33b58eedd71fda73fd9f501d7df
tazro inutano ohta <inutano@gmail.com>
2015-06-07 00:36:19 +0900

    implementing base, refactored database.rb

--- lib/soylattedb.rb
+++ lib/soylattedb.rb
@@ -4,6 +4,7 @@ require File.join(PROJ_ROOT, 'lib', 'soylattedb/reference')
 require File.join(PROJ_ROOT, 'lib', 'soylattedb/scheme')
 require File.join(PROJ_ROOT, 'lib', 'soylattedb/sra')
 require File.join(PROJ_ROOT, 'lib', 'soylattedb/publication')
+require File.join(PROJ_ROOT, 'lib', 'soylattedb/base')
 
 Groonga::Context.default_options = { encoding: :utf8 }
 
